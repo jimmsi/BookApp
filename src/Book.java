f2ecc9851f8605192a1f0ef488391a976cc4144d
Jimmy Emanuelsson <janema2001@skola.goteborg.se>
2023-11-23 09:23:31 +0100

    added book class

---
+++ src/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+
+    private String title;
+    private String author;
+    private String isbn;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Book info:%nTitle: %s%nAuthor: %s%nisbn: %s%n", title, author, isbn);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        List<Book> listOfBooks = new ArrayList<>();
+        listOfBooks.add(new Book("Davids bok", "David", "0471958697"));
+        listOfBooks.add(new Book("Ludwigs bok", "Ludwig", "1234958697"));
+        listOfBooks.add(new Book("Jimmys bok", "Jimmy", "0435958697"));
+
+        for (Book book : listOfBooks) {
+            System.out.println(book);
+        }
+
+
+
 
     }
 
